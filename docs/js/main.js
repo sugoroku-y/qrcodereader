13bbfbb2b0d6dc451bb1e2db3e81cf6edf09b9dd
YEBISUYA Sugoroku <yebisuya@gmail.com>
2020-12-27 23:51:42 +0900

    feat: メニューで開くかコピーするか選択できるように変更

--- docs/js/main.js
+++ docs/js/main.js
@@ -38,17 +38,8 @@ window.addEventListener('load', async () => {
             }
             for (let barcode of barcodes) {
                 const value = barcode.rawValue;
-                const content = (() => {
-                    if (!/^\w+:\/\//.test(value)) {
-                        document.createTextNode(value);
-                    }
-                    const anchor = document.createElement('a');
-                    anchor.textContent = value;
-                    anchor.href = value;
-                    return anchor;
-                })();
                 const li = document.createElement('li');
-                li.appendChild(content);
+                li.appendChild(document.createTextNode(value));
                 result__list.appendChild(li);
             }
             result.classList.add('shown');
@@ -63,5 +54,47 @@ window.addEventListener('load', async () => {
     }
     catch (e) {
         errormessage.textContent = String(e);
+    }
+});
+function offset(element) {
+    return (function sub(e) {
+        const { left, top } = e.offsetParent
+            ? sub(e.offsetParent)
+            : { left: 0, top: 0 };
+        return { left: left + e.offsetLeft, top: top + e.offsetTop };
+    })(element);
+}
+document.addEventListener('click', ev => {
+    var _a, _b, _c;
+    if (!(ev.target instanceof HTMLElement)) {
+        return;
+    }
+    const textElement = ev.target.closest('#result__list > li');
+    if (textElement) {
+        const { left, top } = offset(textElement);
+        menu.setAttribute('data-text', (_a = textElement.textContent) !== null && _a !== void 0 ? _a : '');
+        menu.style.left = `${left}px`;
+        menu.style.top = `${top + textElement.offsetHeight}px`;
+        menu.classList.add('shown');
+        menu__item__navigate.classList.toggle('disabled', !/^\w+:/.test((_b = textElement.textContent) !== null && _b !== void 0 ? _b : ''));
+        ev.preventDefault();
+        ev.stopPropagation();
+        ev.stopImmediatePropagation();
+        return;
+    }
+    menu.classList.remove('shown');
+    const menuItem = ev.target.closest('#menu > div');
+    if (menuItem) {
+        const text = menu.getAttribute('data-text');
+        if (text) {
+            if (menuItem === menu__item__navigate) {
+                location.href = text;
+                return;
+            }
+            if (menuItem === menu__item__copy) {
+                (_c = navigator.clipboard) === null || _c === void 0 ? void 0 : _c.writeText(text);
+                return;
+            }
+        }
     }
 });
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -45,6 +45,9 @@ declare const qrcodereader__video: HTMLVideoElement;
 declare const result: HTMLDivElement;
 declare const result__list: HTMLUListElement;
 declare const result__close: HTMLButtonElement;
+declare const menu: HTMLDivElement;
+declare const menu__item__navigate: HTMLDivElement;
+declare const menu__item__copy: HTMLDivElement;
 
 window.addEventListener('load', async () => {
   if (!navigator.mediaDevices) {
@@ -87,17 +90,8 @@ window.addEventListener('load', async () => {
       }
       for (let barcode of barcodes) {
         const value = barcode.rawValue;
-        const content = (() => {
-          if (!/^\w+:\/\//.test(value)) {
-            document.createTextNode(value);
-          }
-          const anchor = document.createElement('a');
-          anchor.textContent = value;
-          anchor.href = value;
-          return anchor;
-        })();
         const li = document.createElement('li');
-        li.appendChild(content);
+        li.appendChild(document.createTextNode(value));
         result__list.appendChild(li);
       }
       result.classList.add('shown');
@@ -111,5 +105,52 @@ window.addEventListener('load', async () => {
     }
   } catch (e) {
     errormessage.textContent = String(e);
+  }
+});
+
+function offset(element: HTMLElement) {
+  return (function sub(e: HTMLElement): {left: number; top: number} {
+    const {left, top} = e.offsetParent
+      ? sub(e.offsetParent as HTMLElement)
+      : {left: 0, top: 0};
+    return {left: left + e.offsetLeft, top: top + e.offsetTop};
+  })(element);
+}
+
+document.addEventListener('click', ev => {
+  if (!(ev.target instanceof HTMLElement)) {
+    return;
+  }
+  const textElement = ev.target.closest('#result__list > li') as HTMLElement;
+  if (textElement) {
+    const {left, top} = offset(textElement);
+    menu.setAttribute('data-text', textElement.textContent ?? '');
+    menu.style.left = `${left}px`;
+    menu.style.top = `${top + textElement.offsetHeight}px`;
+    menu.classList.add('shown');
+    menu__item__navigate.classList.toggle(
+      'disabled',
+      !/^\w+:/.test(textElement.textContent ?? '')
+    );
+    ev.preventDefault();
+    ev.stopPropagation();
+    ev.stopImmediatePropagation();
+    return;
+  }
+  menu.classList.remove('shown');
+
+  const menuItem = ev.target.closest('#menu > div');
+  if (menuItem) {
+    const text = menu.getAttribute('data-text');
+    if (text) {
+      if (menuItem === menu__item__navigate) {
+        location.href = text;
+        return;
+      }
+      if (menuItem === menu__item__copy) {
+        navigator.clipboard?.writeText(text);
+        return;
+      }
+    }
   }
 });
