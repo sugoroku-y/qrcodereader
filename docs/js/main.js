2bf07fc5d752c3c9395c5d1cd951655b065468c6
YEBISUYA Sugoroku <yebisuya@gmail.com>
2020-12-28 01:26:51 +0900

    コメント追加

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,17 +1,21 @@
 "use strict";
+// BarcodeDetectorのための定義・宣言 ここから
 window.addEventListener('load', async () => {
+    // navigator.mediaDevicesがなければエラー
     if (!navigator.mediaDevices) {
         errormessage.textContent =
             'navigator.mediaDevices is not supported by this browser.';
         return;
     }
-    if (window.BarcodeDetector == undefined) {
+    // BarcodeDetectorがなければエラー
+    if (!window.BarcodeDetector) {
         errormessage.textContent =
             'BarcodeDetector is not supported by this browser.';
         return;
     }
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({
+        // カメラからのストリームを取得してvideoに接続
+        qrcodereader__video.srcObject = await navigator.mediaDevices.getUserMedia({
             audio: false,
             video: {
                 facingMode: {
@@ -19,34 +23,41 @@ window.addEventListener('load', async () => {
                 },
             },
         });
-        qrcodereader__video.srcObject = stream;
-        await new Promise(r => {
-            qrcodereader__video.onloadedmetadata = _e => {
-                qrcodereader__video.play();
-                r();
-            };
-        });
+        // 読み込み完了を待つ
+        await new Promise(r => qrcodereader__video.addEventListener('loadedmetadata', () => r()));
+        // 再生開始
+        await qrcodereader__video.play();
+        // バーコード読み取り機能の準備
         const barcodeDetector = new BarcodeDetector();
         while (true) {
+            // バーコード読み取り
             const barcodes = await barcodeDetector.detect(qrcodereader__video);
+            // バーコードが読み取れなかったら空の配列が返る
             if (!barcodes.length) {
+                // 0.2秒待機してもう一度
                 await new Promise(r => setTimeout(r, 200));
                 continue;
             }
+            // 一旦前回の読み取り結果を削除
             while (result__list.firstChild) {
                 result__list.removeChild(result__list.firstChild);
             }
+            // 今回の読み取り結果を反映
             for (let barcode of barcodes) {
                 const value = barcode.rawValue;
                 const li = document.createElement('li');
                 li.appendChild(document.createTextNode(value));
                 result__list.appendChild(li);
             }
+            // 読み取り結果を表示
             result.classList.add('shown');
+            // 読み取り結果の閉じるボタンがクリックされるまで待機
             await new Promise(r => {
                 result__close.addEventListener('click', function handler() {
+                    // クリックされたら読み取り結果を非表示
                     result.classList.remove('shown');
                     r();
+                    // クリックハンドラを解除
                     result__close.removeEventListener('click', handler);
                 });
             });
@@ -56,41 +67,57 @@ window.addEventListener('load', async () => {
         errormessage.textContent = String(e);
     }
 });
+/**
+ * body要素左上からの位置を取得
+ * @param element
+ */
 function offset(element) {
-    return (function sub(e) {
-        const { left, top } = e.offsetParent
-            ? sub(e.offsetParent)
-            : { left: 0, top: 0 };
-        return { left: left + e.offsetLeft, top: top + e.offsetTop };
-    })(element);
+    const r = { left: 0, top: 0 };
+    while (element) {
+        r.left += element.offsetLeft;
+        r.top += element.offsetTop;
+        element = element.offsetParent;
+    }
+    return r;
 }
 document.addEventListener('click', ev => {
     var _a, _b, _c;
     if (!(ev.target instanceof HTMLElement)) {
         return;
     }
+    // 読み取り結果の一項目がクリックされたとき
     const textElement = ev.target.closest('#result__list > li');
     if (textElement) {
+        // 項目の左上の座標を取得
         const { left, top } = offset(textElement);
+        // 項目のテキストを属性値に設定
         menu.setAttribute('data-text', (_a = textElement.textContent) !== null && _a !== void 0 ? _a : '');
+        // メニューを項目の左下に配置
         menu.style.left = `${left}px`;
         menu.style.top = `${top + textElement.offsetHeight}px`;
+        // メニューを表示
         menu.classList.add('shown');
+        // URLっぽいテキストでないときは「開く」をグレーアウト
         menu__item__navigate.classList.toggle('disabled', !/^\w+:/.test((_b = textElement.textContent) !== null && _b !== void 0 ? _b : ''));
+        // デフォルトのクリックを処理しないように
         ev.preventDefault();
         ev.stopPropagation();
         ev.stopImmediatePropagation();
         return;
     }
+    // 読み取り結果の一項目以外のところがクリックされたら、メニューを閉じる
     menu.classList.remove('shown');
+    // メニューの項目がクリックされたとき
     const menuItem = ev.target.closest('#menu > div');
     if (menuItem) {
         const text = menu.getAttribute('data-text');
         if (text) {
+            // 開くがクリックされたとき
             if (menuItem === menu__item__navigate) {
                 location.href = text;
                 return;
             }
+            // コピーがクリックされたとき
             if (menuItem === menu__item__copy) {
                 (_c = navigator.clipboard) === null || _c === void 0 ? void 0 : _c.writeText(text);
                 return;
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,6 @@
+
+// BarcodeDetectorのための定義・宣言 ここから
+
 interface Point2D {
   x: number;
   y: number;
@@ -39,7 +42,9 @@ interface Window {
   BarcodeDetector?: BarcodeDetectorConstructor;
 }
 declare const BarcodeDetector: BarcodeDetectorConstructor;
+// BarcodeDetectorのための型定義・宣言 ここまで
 
+// html内でID指定した要素
 declare const errormessage: HTMLDivElement;
 declare const qrcodereader__video: HTMLVideoElement;
 declare const result: HTMLDivElement;
@@ -50,20 +55,23 @@ declare const menu__item__navigate: HTMLDivElement;
 declare const menu__item__copy: HTMLDivElement;
 
 window.addEventListener('load', async () => {
+  // navigator.mediaDevicesがなければエラー
   if (!navigator.mediaDevices) {
     errormessage.textContent =
       'navigator.mediaDevices is not supported by this browser.';
     return;
   }
 
-  if (window.BarcodeDetector == undefined) {
+  // BarcodeDetectorがなければエラー
+  if (!window.BarcodeDetector) {
     errormessage.textContent =
       'BarcodeDetector is not supported by this browser.';
     return;
   }
 
   try {
-    const stream = await navigator.mediaDevices.getUserMedia({
+    // カメラからのストリームを取得してvideoに接続
+    qrcodereader__video.srcObject = await navigator.mediaDevices.getUserMedia({
       audio: false,
       video: {
         facingMode: {
@@ -71,34 +79,46 @@ window.addEventListener('load', async () => {
         },
       },
     });
-    qrcodereader__video.srcObject = stream;
-    await new Promise<void>(r => {
-      qrcodereader__video.onloadedmetadata = _e => {
-        qrcodereader__video.play();
-        r();
-      };
-    });
+    // 読み込み完了を待つ
+    await new Promise<void>(r =>
+     
+     
+     
+      qrcodereader__video.addEventListener('loadedmetadata', () => r())
+    );
+    // 再生開始
+    await qrcodereader__video.play();
+    // バーコード読み取り機能の準備
     const barcodeDetector = new BarcodeDetector();
     while (true) {
+      // バーコード読み取り
       const barcodes = await barcodeDetector.detect(qrcodereader__video);
+      // バーコードが読み取れなかったら空の配列が返る
       if (!barcodes.length) {
+        // 0.2秒待機してもう一度
         await new Promise(r => setTimeout(r, 200));
         continue;
       }
+      // 一旦前回の読み取り結果を削除
       while (result__list.firstChild) {
         result__list.removeChild(result__list.firstChild);
       }
+      // 今回の読み取り結果を反映
       for (let barcode of barcodes) {
         const value = barcode.rawValue;
         const li = document.createElement('li');
         li.appendChild(document.createTextNode(value));
         result__list.appendChild(li);
       }
+      // 読み取り結果を表示
       result.classList.add('shown');
+      // 読み取り結果の閉じるボタンがクリックされるまで待機
       await new Promise<void>(r => {
         result__close.addEventListener('click', function handler() {
+          // クリックされたら読み取り結果を非表示
           result.classList.remove('shown');
           r();
+          // クリックハンドラを解除
           result__close.removeEventListener('click', handler);
         });
       });
@@ -108,45 +128,61 @@ window.addEventListener('load', async () => {
   }
 });
 
+/**
+ * body要素左上からの位置を取得
+ * @param element 
+ */
 function offset(element: HTMLElement) {
-  return (function sub(e: HTMLElement): {left: number; top: number} {
-    const {left, top} = e.offsetParent
-      ? sub(e.offsetParent as HTMLElement)
-      : {left: 0, top: 0};
-    return {left: left + e.offsetLeft, top: top + e.offsetTop};
-  })(element);
+  const r = {left: 0, top: 0};
+  while (element) {
+    r.left += element.offsetLeft;
+    r.top += element.offsetTop;
+    element = element.offsetParent as HTMLElement;
+  }
+  return r;
 }
 
 document.addEventListener('click', ev => {
   if (!(ev.target instanceof HTMLElement)) {
     return;
   }
+  // 読み取り結果の一項目がクリックされたとき
   const textElement = ev.target.closest('#result__list > li') as HTMLElement;
   if (textElement) {
+    // 項目の左上の座標を取得
     const {left, top} = offset(textElement);
+    // 項目のテキストを属性値に設定
     menu.setAttribute('data-text', textElement.textContent ?? '');
+    // メニューを項目の左下に配置
     menu.style.left = `${left}px`;
     menu.style.top = `${top + textElement.offsetHeight}px`;
+    // メニューを表示
     menu.classList.add('shown');
+    // URLっぽいテキストでないときは「開く」をグレーアウト
     menu__item__navigate.classList.toggle(
       'disabled',
       !/^\w+:/.test(textElement.textContent ?? '')
     );
+    // デフォルトのクリックを処理しないように
     ev.preventDefault();
     ev.stopPropagation();
     ev.stopImmediatePropagation();
     return;
   }
+  // 読み取り結果の一項目以外のところがクリックされたら、メニューを閉じる
   menu.classList.remove('shown');
 
+  // メニューの項目がクリックされたとき
   const menuItem = ev.target.closest('#menu > div');
   if (menuItem) {
     const text = menu.getAttribute('data-text');
     if (text) {
+      // 開くがクリックされたとき
       if (menuItem === menu__item__navigate) {
         location.href = text;
         return;
       }
+      // コピーがクリックされたとき
       if (menuItem === menu__item__copy) {
         navigator.clipboard?.writeText(text);
         return;
